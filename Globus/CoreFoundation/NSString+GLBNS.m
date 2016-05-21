2821aa36213c18b184e09834c3409eb401c757e7
Alexander Trifonov (fgengine) <a.trifonov@globus-ltd.com, a.trifonov@yandex-team.ru, fgengine@gmail.com>
2016-05-21 19:02:01 +0300

    Fix string encoding

--- Globus/CoreFoundation/NSString+GLBNS.m
+++ Globus/CoreFoundation/NSString+GLBNS.m
@@ -95,17 +95,30 @@ static UInt32 GLB_CRC32(const char* aString) {
     return [[NSString  stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16], result[17], result[18], result[19], result[20], result[21], result[22], result[23], result[24], result[25], result[26], result[27], result[28], result[29], result[30], result[31]] lowercaseString];
 }
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
 - (NSString*)glb_stringByDecodingURLFormat {
-    return [self stringByRemovingPercentEncoding];
+    NSString* result = nil;
+    CFStringRef string = CFURLCreateStringByReplacingPercentEscapesUsingEncoding(NULL, (__bridge CFStringRef)self, CFSTR(""), kCFStringEncodingUTF8);
+    if(string != nil) {
+        result = [NSString stringWithString:(__bridge NSString*)string];
+        CFRelease(string);
+    }
+    return result;
 }
 
 - (NSString*)glb_stringByEncodingURLFormat {
-    static NSCharacterSet* characterSet = nil;
-    if(characterSet == nil) {
-        characterSet = [NSCharacterSet characterSetWithCharactersInString:@"!*'();:@&=+$,/?%#[]"];
+    NSString* result = nil;
+    CFStringRef string = CFURLCreateStringByAddingPercentEscapes(NULL, (__bridge CFStringRef)self, NULL, CFSTR("!*'();:@&=+$,/?%#[]"), kCFStringEncodingUTF8);
+    if(string != nil) {
+        result = [NSString stringWithString:(__bridge NSString*)string];
+        CFRelease(string);
     }
-    return [self stringByAddingPercentEncodingWithAllowedCharacters:characterSet];
+    return result;
 }
+
+#pragma clang diagnostic pop
 
 - (NSDictionary*)glb_dictionaryFromQueryComponents {
     NSMutableDictionary* queryComponents = NSMutableDictionary.dictionary;
