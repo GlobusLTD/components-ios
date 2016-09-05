e7bcd8779bab280354147186914924d76eeafdb3
Andrew Vanyurin <dddron@gmail.com>
2016-09-05 20:54:29 +0300

    NSFileManager was documented

--- Globus/CoreFoundation/FileManager/NSFileManager+GLBNS.h
+++ Globus/CoreFoundation/FileManager/NSFileManager+GLBNS.h
@@ -16,10 +16,46 @@
 
 /*--------------------------------------------------*/
 
+/**
+ * @file NSFileManager+GLBNS.h
+ * @class NSFileManager+GLBNS
+ * @classdesign It is a category
+ * @helps NSFileManager
+ * @brief Many helpful methods for NSFileManager support.
+ * @discussion NSFileManager support for directory handle.
+ * @remark Very useful methods ;)
+ * @version 0.1
+ */
 @interface NSFileManager (GLB_NS)
 
+/**
+ * @brief Document directory.
+ * @discussion Creates a path string for the document directory.
+ * @return Document directory path string.
+ * @code
+ * NSString* s = [NSFileManager glb_documentDirectory];
+ * @endcode
+ */
 + (NSString* _Nullable)glb_documentDirectory;
+
+/**
+ * @brief Library directory.
+ * @discussion Various user-visible documentation, support, and configuration files (/Library).
+ * @return Library directory path string.
+ * @code
+ * NSString* s = [NSFileManager glb_libraryDirectory];
+ * @endcode
+ */
 + (NSString* _Nullable)glb_libraryDirectory;
+
+/**
+ * @brief Cache directory.
+ * @discussion Location of cache files.
+ * @return Cache directory path string.
+ * @code
+ * NSString* s = [NSFileManager glb_cachesDirectory];
+ * @endcode
+ */
 + (NSString* _Nullable)glb_cachesDirectory;
 
 @end
