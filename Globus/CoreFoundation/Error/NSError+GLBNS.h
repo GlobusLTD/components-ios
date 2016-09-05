4b335049e33406b5fc3834dbc8a05503b8541139
Andrew Vanyurin <dddron@gmail.com>
2016-09-05 20:45:27 +0300

    glb error was documented

--- Globus/CoreFoundation/Error/NSError+GLBNS.h
+++ Globus/CoreFoundation/Error/NSError+GLBNS.h
@@ -16,11 +16,56 @@
 
 /*--------------------------------------------------*/
 
+/**
+ * @file NSError+GLBNS.h
+ * @class NSError+GLBNS
+ * @classdesign It is a category
+ * @helps NSError
+ * @brief Many helpful methods for NSError support.
+ * @discussion NSError support for handle URL errors.
+ * @remark Very useful methods ;)
+ * @version 0.1
+ */
 @interface NSError (GLB_NS)
 
+/**
+ * @brief URL loading system errors.
+ * @discussion Is this the URL loading system errors.
+ * @return Is the error of URL error.
+ * @code
+ * if ([error glb_isURLError] == NO) {}
+ * @endcode
+ */
 - (BOOL)glb_isURLError;
+
+/**
+ * @brief Connection not established.
+ * @discussion Is this an internet connection not established error.
+ * @return Is connection established.
+ * @code
+ * if ([error glb_URLErrorConnectedToInternet] == YES) {}
+ * @endcode
+ */
 - (BOOL)glb_URLErrorConnectedToInternet;
+
+/**
+ * @brief An asynchronous load is canceled.
+ * @discussion An asynchronous load is canceled.
+ * @return Is loading canceled.
+ * @code
+ * if ([error glb_URLErrorCancelled] == YES) {}
+ * @endcode
+ */
 - (BOOL)glb_URLErrorCancelled;
+
+/**
+ * @brief Is times out.
+ * @discussion Is times over.
+ * @return Is times out.
+ * @code
+ * if ([error glb_URLErrorTimedOut] == YES) {}
+ * @endcode
+ */
 - (BOOL)glb_URLErrorTimedOut;
 
 @end
